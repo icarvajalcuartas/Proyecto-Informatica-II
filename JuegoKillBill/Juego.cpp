da0ffdc6adadf0f0c4c7296b8ad0838448ad13eb
icarvajal.cuartas <icarvajal.cuartas@udea.edu.co>
2026-06-04 23:16:34 -0500

    Conexion de las clases para repetir lo que se usaba con la clase de prueba

--- JuegoKillBill/Juego.cpp
+++ JuegoKillBill/Juego.cpp
@@ -1,8 +1,75 @@
 #include "Juego.h"
 
-Juego::Juego() {}
+bool Juego::eventFilter(QObject *obj, QEvent *event)
+{
+    if(event->type() == QEvent::KeyPress)
+    {
+        QKeyEvent* tecla = static_cast<QKeyEvent*>(event);
+        if(tecla->isAutoRepeat())
+            return true;
 
-unsigned short Juego::getNivelActual()
+        if(nivelActual < niveles.size())
+        {
+            niveles[nivelActual]->inputJugador(tecla);
+        }
+    }
+    else if(event->type() == QEvent::KeyRelease)
+    {
+        QKeyEvent* tecla = static_cast<QKeyEvent*>(event);
+        if(tecla->isAutoRepeat())
+            return true;
+
+        niveles[nivelActual]->inputJugadorLiberada(tecla);
+    }
+
+    return QObject::eventFilter(obj,event);
+}
+void Juego::actualizarJuego()
+{
+    if(nivelActual < niveles.size())
+    {
+        niveles[nivelActual]->actualizar(0.016f);
+    }
+}
+Juego::Juego() :nivelActual(0),estadoJuegoActual(EstadoJuego::Menu){
+
+    escena = new QGraphicsScene(this);
+    timer = new QTimer(this);
+    reproductor = new QMediaPlayer(this);
+    salidaAudio = new QAudioOutput(this);
+    reproductor->setAudioOutput(salidaAudio);
+    niveles.append(new Nivel(1, escena));
+    connect(timer,&QTimer::timeout,this,&Juego::actualizarJuego);
+    timer->start(16);
+    inicialNivel(0);
+}
+
+void Juego::inicialNivel(unsigned short nivel)
+{
+    if(nivel >= niveles.size())
+        return;
+    escena->clear();
+    nivelActual = nivel;
+    niveles[nivelActual]->cargar();
+}
+
+unsigned short int Juego::getNivelActual() const
 {
     return nivelActual;
 }
+void Juego::reproducirMusica(const QString& ruta)
+{
+    reproductor->setSource(QUrl(ruta));
+    reproductor->play();
+}
+QGraphicsScene* Juego::getEscena() const
+{
+    return escena;
+}
+Juego::~Juego()
+{
+    qDeleteAll(niveles);
+    niveles.clear();
+}
+
+
--- JuegoKillBill/main.cpp
+++ JuegoKillBill/main.cpp
@@ -8,32 +8,22 @@
 #include "Bride.h"
 #include <QDir>
 #include "test.h"
+#include"Juego.h"
 
 
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
     qDebug() << QDir::currentPath();
     //ventana
-    QGraphicsView* ventana;
-
-    //fin ventana
-    //scene y poner a bride
-    QGraphicsScene* scene = new QGraphicsScene();
-    ventana = new QGraphicsView(scene);
-    ventana->setWindowTitle("Kill Bill Kendo");
+    Juego *juego = new Juego();
+    QGraphicsView* ventana=new QGraphicsView(juego->getEscena());
     ventana->resize(1200,630);
-
-    Bride* bride = new Bride(100,100);
-
-    scene->addItem(bride);
-    bride->setPos(100,100);
-
-    //prueba de perifericos
-    Test* input = new Test(bride);
-    ventana->installEventFilter(input);
-    // fin prueba perifericos
+    ventana->installEventFilter(juego);
+    ventana->setWindowTitle("Kill Bill Kendo");
+    //fin ventana
 
     ventana->show();
     return a.exec();
